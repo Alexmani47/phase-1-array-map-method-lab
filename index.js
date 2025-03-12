c8a815cab5c636e3660530687fc4ff1fd8e97eca
Alex <alex.mani@student.moringaschool.com>
2025-03-12 23:08:32 +0300

    Done

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(` `).map(word => word.charAt(0).toUpperCase() +  word.slice(1))
+    .join(` `);
+  });
 }
+console.log(titleCased());
+//const skiSchool = ["Aki", "guadalupe", "lei", "aalam"];
+//const rollCall = skiSchool.map(function (student) {
+  //return student + "the skier";
+//});
+//console.log(rollCall);
+//const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
+/*const rollCall = skiSchool.map((student) => student + "the skier");
+console.log(rollCall);
+
+const robots = [
+  { name: "Johnny 5", modes: 5, isActivated: false},
+  { name: "C2PO", modes: 3, isActivated: false},
+  { name: "Sonny", modes: 2.5, isActivated: false},
+  { name: "Baymax", modes: 1.5, isActivated: false},
+];
+const activatedRobots = robots.map((robot) => {
+  return Object.assign({}, robot, {
+    modes: robot.modes * 2,
+    isActivated: true,
+  });
+});
+console.log(activatedRobots)*/
